bd49faceeb648219592442ec699fe663abdff4b1
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2025-01-06 18:33:34 -0500

    Cleanup

--- operators/ShapeKeysOps.py
+++ operators/ShapeKeysOps.py
@@ -24,20 +24,20 @@ class ZeniTools_OP_BatchShapeKeyTransfer(bpy.types.Operator):
 
         target_meshes = []
         for obj in bpy.context.selected_objects:
-            if obj == source_mesh:
-                self.report({'WARNING'},  "Can not Transfered Shape Keys to self.")
-                continue
             if isinstance(obj.data, bpy.types.Mesh):
+                if obj == source_mesh:
+                    self.report({'WARNING'},  "Can't transfer Shape Keys to self. Skipping")
+                    continue
                 target_meshes.append(obj)
 
-        if len(context.selected_objects) == 0:
+        if len(target_meshes) == 0:
             self.report({'ERROR'},  "No mesh selected")
             return {'CANCELLED'}
 
-        shapekeySkips = 0
+        shapeKeysSkipped = 0
         for target_mesh in target_meshes:
             try:
-                if target_mesh == None:
+                if target_mesh == None: # This should be impossible. If it happens uhhhh. Explode.
                     self.report({'WARNING'},  "Target and/or source cannot be null.")
 
                 self.report({'INFO'},  f"Transfering Shape Keys from {source_mesh.name} to {target_mesh.name}.")
@@ -67,9 +67,9 @@ class ZeniTools_OP_BatchShapeKeyTransfer(bpy.types.Operator):
                             continue
                         if (shapekey.value != 0):
                             self.report({'WARNING'},  f'Shape key "{shapekey.name}" on {source_mesh.name} is not 0. Skipped')
-                            shapekeySkips += 1
+                            shapeKeysSkipped += 1
                             continue
-                        self.report({'INFO'}, shapekey.name)
+                        
                         shapekey.value = 1
                         surf_def_mod.name = shapekey.name
 
@@ -77,7 +77,7 @@ class ZeniTools_OP_BatchShapeKeyTransfer(bpy.types.Operator):
                         shapekey.value = 0 # reset
                     except Exception as ex: 
                         self.report({'ERROR'},  f'Error occured while tyrying to copy a shape key: \n {ex}') 
-                        shapekeySkips += 1
+                        shapeKeysSkipped += 1
 
                 surf_def_mod.name = modifierName
                 bpy.ops.object.modifier_remove(modifier=modifierName)
@@ -85,7 +85,7 @@ class ZeniTools_OP_BatchShapeKeyTransfer(bpy.types.Operator):
                 self.report({'ERROR'},  f'Error occured while trying to interact with a mesh: \n {ex}') 
                 return {'CANCELLED'}
             
-        endTextSkip = f" {shapekeySkips} shape keys were skipped." if shapekeySkips > 0 else ""
+        endTextSkip = f" {shapeKeysSkipped} Shape Keys were skipped." if shapeKeysSkipped > 0 else ""
         endTextMode = "selected meshes" if props.ZeniTools_ShapeKeys_ApplyToAllSelected else target_meshes[0].name
         self.report({'INFO'},  f"Shape Keys transferred from {source_mesh.name} to {endTextMode}. {endTextSkip}")
         
@@ -95,11 +95,11 @@ class Properties(bpy.types.PropertyGroup):
     bpy.types.Scene.ZeniTools_ShapeKeys_Source_Object = bpy.props.PointerProperty(
         name="Source Object",
         type=bpy.types.Object,
-        description="Mesh with shapekeys you want trasnfered",
+        description="Mesh with shapekeys you want transfered.",
     )
     bpy.types.Scene.ZeniTools_ShapeKeys_Vertex_Group = bpy.props.StringProperty(
         name="Vertex Group Mask",
-        description="Optional vertex Group mask for shapekey transfer. Ignored if applying to selected is enabled.",
+        description="Optional vertex Group mask for shapekey transfer.",
     )
     bpy.types.Scene.ZeniTools_ShapeKeys_Vertex_Group_Invert = bpy.props.BoolProperty(
         name="Invert Vertex Group",
